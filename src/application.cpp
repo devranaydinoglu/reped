9700be789512d97e7b5d0f9cbd1beebb1f03f997
devranaydinoglu <d.s.aydinoglu@gmail.com>
2025-08-07 21:30:30 +0200

    Integrate text engine and controller

--- src/application.cpp
+++ src/application.cpp
@@ -2,15 +2,25 @@
 
 #include "application.h"
 #include "./ui/window.h"
+#include "./text_engine/text_engine.h"
+#include "./controller/controller.h"
 
 Application::Application()
-    : client(nullptr), server(nullptr)
+    : client(nullptr), server(nullptr), textEngine(std::make_unique<TextEngine>()), 
+        controller(std::make_unique<Controller>())
 {
+    controller->setTextEngine(textEngine.get());
+    
+    
+    // Setup window callbacks
     window.setOnSetupCompletedCallback([this] (AppMode appMode, const uint16_t port, const std::string& serverAddress)
     {
         this->onSetupCompleted(appMode, port, serverAddress);
     });
     
+
+    window.setEditorController(controller.get());
+
     window.render();
 }
 
@@ -20,6 +30,7 @@ void Application::onSetupCompleted(AppMode appMode, const uint16_t port, const s
     {
         case AppMode::CLIENT:
             client = std::make_unique<Client>(port, serverAddress);
+            controller->setClient(client.get());
             break;
         case AppMode::SERVER:
             server = std::make_unique<Server>(port, serverAddress);
--- src/application.h
+++ src/application.h
@@ -5,6 +5,8 @@
 #include "./ui/window.h"
 #include "./networking/server.h"
 #include "./networking/client.h"
+#include "./text_engine/text_engine.h"
+#include "./controller/controller.h"
 
 enum class AppMode
 {
@@ -18,10 +20,14 @@ class Application
 public:
     std::unique_ptr<Client> client;
     std::unique_ptr<Server> server;
+    std::unique_ptr<Controller> controller;
+    std::unique_ptr<TextEngine> textEngine;
     
     Window window;
 
 public:
     Application();
+
+private:
     void onSetupCompleted(AppMode appMode, const uint16_t port, const std::string& serverAddress);
 };
