a34da4e46d58475f896e37cdcbb5950cdb702129
devranaydinoglu <d.s.aydinoglu@gmail.com>
2025-08-07 21:29:26 +0200

    Replaced ImGui::TextInputMultiline with custom editor frontend

--- src/ui/editor.cpp
+++ src/ui/editor.cpp
@@ -1,39 +1,176 @@
 #include "editor.h"
 #include "imgui.h"
+#include "../controller/controller.h"
+#include "../text_engine/input_events.h"
+
+#include <iostream>
 
 Editor::Editor()
+    : controller(nullptr), cursorLastMovedTime(0.0f)
 {
-
 }
 
-void Editor::showEditor(bool *open)
+void Editor::showEditor(bool* open)
 {
     IMGUI_CHECKVERSION();
 
-    ImGuiWindowFlags windowFlags = 0;
-    windowFlags |= ImGuiWindowFlags_NoTitleBar;
-    windowFlags |= ImGuiWindowFlags_NoMove;
-    windowFlags |= ImGuiWindowFlags_NoResize;
-
+    ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize;
     ImGuiIO& io = ImGui::GetIO();
-    ImGui::SetNextWindowSize(ImVec2(io.DisplaySize.x, io.DisplaySize.y));
+    ImVec2 editorSize(io.DisplaySize.x, io.DisplaySize.y);
+    ImGui::SetNextWindowSize(editorSize);
     ImGui::SetNextWindowPos(ImVec2(0, 0));
 
-    // Start of the window
-    ImGui::Begin("Editor", open, windowFlags);
+    ImGui::Begin("CustomEditor", open, windowFlags);
 
-    float margin = 20.0f;
+    // Draw background
+    ImDrawList* drawList = ImGui::GetWindowDrawList();
+    ImVec2 contentAreaOrigin = ImGui::GetCursorScreenPos();
+    ImVec2 contentRegionAvail = ImGui::GetContentRegionAvail();
+    ImVec2 pMax = ImVec2(contentAreaOrigin.x + contentRegionAvail.x, contentAreaOrigin.y + contentRegionAvail.y);
+    drawList->AddRectFilled(contentAreaOrigin, pMax, IM_COL32(30, 30, 30, 255));
 
-    ImGuiStyle& style = ImGui::GetStyle();
-    ImVec2 padding = style.WindowPadding;
-    ImVec2 windowSize = ImGui::GetWindowSize();
-    ImVec2 editorSize(windowSize.x - 2 * margin, windowSize.y - 2 * margin);
-    ImVec2 offset(margin, margin);
+    // Input capture
+    ImGui::SetCursorScreenPos(contentAreaOrigin);
+    ImGui::InvisibleButton("EditorInputCapture", contentRegionAvail, ImGuiButtonFlags_MouseButtonLeft);
+    ImGui::SetItemDefaultFocus();
+    ImGui::SetKeyboardFocusHere();
+
+    bool editorIsActive = ImGui::IsItemFocused();
+    ImFont* font = ImGui::GetFont();
+    float lineHeight = ImGui::GetTextLineHeight();
+    float charWidth = font->CalcTextSizeA(font->LegacySize, FLT_MAX, 0.0f, "A").x;
+
+    float baseX = contentAreaOrigin.x + 5.0f;
+    float baseY = contentAreaOrigin.y + 5.0f;
+
+    std::string text = controller->getText();
+    size_t cursorPos = controller->getCursorPosition();
     
-    ImGui::SetCursorPos(offset);
+    // INPUT HANDLING
+    if (editorIsActive)
+    {
+        for (int i = 0; i < io.InputQueueCharacters.Size; ++i)
+        {
+            ImWchar c = io.InputQueueCharacters[i];
+            if (c >= 32 && c != 127) // Printable ASCII
+            {
+                controller->handleTextInputEvent(TextInputEvent(TextInputEventType::INSERT, c, cursorPos));
+                controller->handleCursorInputEvent(CursorInputEvent(cursorPos + 1));
+                cursorPos = controller->getCursorPosition();
+                cursorLastMovedTime = ImGui::GetTime();
+            }
+        }
+
+        if (ImGui::IsKeyPressed(ImGuiKey_LeftArrow) && cursorPos > 0)
+        {
+            controller->handleCursorInputEvent(CursorInputEvent(cursorPos - 1));
+            cursorPos = controller->getCursorPosition();
+            cursorLastMovedTime = ImGui::GetTime();
+        }
+        else if (ImGui::IsKeyPressed(ImGuiKey_RightArrow) && cursorPos < text.size())
+        {
+            controller->handleCursorInputEvent(CursorInputEvent(cursorPos + 1));
+            cursorPos = controller->getCursorPosition();
+            cursorLastMovedTime = ImGui::GetTime();
+        }
+
+        auto it = std::upper_bound(lineStartOffsets.begin(), lineStartOffsets.end(), cursorPos);
+        size_t currentLine = std::distance(lineStartOffsets.begin(), it) - 1;
+        size_t column = cursorPos - lineStartOffsets[currentLine];
+
+        if (ImGui::IsKeyPressed(ImGuiKey_UpArrow) && currentLine > 0)
+        {
+            size_t prevLineStart = lineStartOffsets[currentLine - 1];
+            size_t prevLineEnd = lineStartOffsets[currentLine];
+            controller->handleCursorInputEvent(CursorInputEvent(prevLineStart + std::min(column, prevLineEnd - prevLineStart - 1)));
+            cursorPos = controller->getCursorPosition();
+            cursorLastMovedTime = ImGui::GetTime();
+        }
+        else if (ImGui::IsKeyPressed(ImGuiKey_DownArrow) && currentLine + 1 < lineStartOffsets.size() - 1)
+        {
+            size_t nextLineStart = lineStartOffsets[currentLine + 1];
+            size_t nextLineEnd = lineStartOffsets[currentLine + 2];
+            controller->handleCursorInputEvent(CursorInputEvent(nextLineStart + std::min(column, nextLineEnd - nextLineStart - 1)));
+            cursorPos = controller->getCursorPosition();
+            cursorLastMovedTime = ImGui::GetTime();
+        }
+
+        if (ImGui::IsKeyPressed(ImGuiKey_Backspace) && cursorPos > 0)
+        {
+            controller->handleTextInputEvent(TextInputEvent(TextInputEventType::DELETE, '\0', cursorPos));
+            controller->handleCursorInputEvent(CursorInputEvent(cursorPos - 1));
+            cursorPos = controller->getCursorPosition();
+            cursorLastMovedTime = ImGui::GetTime();
+        }
+
+        if (ImGui::IsKeyPressed(ImGuiKey_Enter))
+        {
+            controller->handleTextInputEvent(TextInputEvent(TextInputEventType::INSERT, '\n', cursorPos));
+            controller->handleCursorInputEvent(CursorInputEvent(cursorPos + 1));
+            cursorPos = controller->getCursorPosition();
+            cursorLastMovedTime = ImGui::GetTime();
+        }
+    }
+
+    cursorPos = controller->getCursorPosition();
+
+    // Re-fetch updated text after input handling
+    text = controller->getText();
     
-    char editorTextInput[4096];
-    ImGui::InputTextMultiline("##EditorText", editorTextInput, IM_ARRAYSIZE(editorTextInput), editorSize);
+    // Build lineStartOffsets after input changed text
+    lineStartOffsets.clear();
+    lineStartOffsets.emplace_back(0);
+    for (size_t i = 0; i < text.size(); i++)
+        if (text[i] == '\n')
+            lineStartOffsets.emplace_back(i + 1);
+    if (lineStartOffsets.empty())
+        lineStartOffsets.emplace_back(text.size());
+
+    // RENDERING
+    text = controller->getText();
+    
+    size_t numCharsRendered = 0;
+
+    for (size_t lineIndex = 0; lineIndex <= lineStartOffsets.size() - 1; ++lineIndex)
+    {
+        size_t lineStart = lineStartOffsets[lineIndex];
+        size_t lineEnd = lineStartOffsets[lineIndex + 1];
+        std::size_t len;
+        if (lineEnd == 0)
+            len = text.size();
+        else
+            len = lineEnd;
+        std::string lineBuffer = text.substr(lineStart, len);
+        ImVec2 linePos = ImVec2(baseX, baseY + lineIndex * lineHeight);
+        drawList->AddText(linePos, IM_COL32_WHITE, lineBuffer.c_str());
+        numCharsRendered += lineBuffer.size();
+    }
+
+    for (auto s : lineStartOffsets)
+        std::cout << "Off: " << s << "\n";
+
+    // Cursor line/column
+    auto it = std::upper_bound(lineStartOffsets.begin(), lineStartOffsets.end(), cursorPos);
+    size_t cursorLine = std::distance(lineStartOffsets.begin(), it) - 1;
+    size_t cursorColumn = cursorPos - lineStartOffsets[cursorLine];
+
+    std::cout << "Cursor line: " << cursorLine << "\n";
+    std::cout << "Cursor column: " << cursorColumn << "\n";
+
+    float visualCursorX = baseX + cursorColumn * charWidth;
+    float visualCursorY = baseY + cursorLine * lineHeight;
+
+    // Draw blinking cursor
+    float timeSinceMove = ImGui::GetTime() - cursorLastMovedTime;
+    bool showCursor = (timeSinceMove < 0.5f) || (static_cast<int>((ImGui::GetTime() * 2.0f)) % 2 == 0);
+
+    if (showCursor)
+    {
+        drawList->AddLine(
+            ImVec2(visualCursorX, visualCursorY),
+            ImVec2(visualCursorX, visualCursorY + lineHeight),
+            IM_COL32_WHITE, 1.0f);
+    }
 
     ImGui::End();
 }
--- src/ui/editor.h
+++ src/ui/editor.h
@@ -1,8 +1,23 @@
 #pragma once
 
+#include <cstddef>
+#include <vector>
+
+struct ImGuiInputTextCallbackData;
+class Controller;
+
 class Editor
 {
+private:
+    Controller* controller;
+
+    // Character offset of the start of each line
+    std::vector<std::size_t> lineStartOffsets;
+
+    float cursorLastMovedTime;
+
 public:
     Editor();
     void showEditor(bool* open);
+    void setController(Controller* controller) { this->controller = controller; }
 };
